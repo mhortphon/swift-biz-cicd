1daa33c275fea1b003e6872ab3fe79835155d20c
Meng Hort <mhort.phon@gmail.com>
2024-09-03 22:51:17 +0700

    first commit

---
+++ src/main/java/com/swiftbizcentral/spring_boot_cicd/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.swiftbizcentral.spring_boot_cicd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/welcome")
+    public String getMessage() {
+        return "Hello Swift Biz Central.";
+    }
+}
